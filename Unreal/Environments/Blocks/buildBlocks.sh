9a5157afa2a1a46c414b00e9d3e9633eb2de32f0
TravisMoleski <tm769613@ohio.edu>
2022-08-23 12:27:29 -0400

    added build command for UE 2.27

---
+++ Unreal/Environments/Blocks/buildBlocks.sh
@@ -0,0 +1,6 @@
+# /home/travis/Mount/Storage/SharedHDD/Linux/UnrealEngine/Unreal427/UnrealEngine/GenerateProjectFiles.sh /home/travis/Mount/Storage/SharedHDD/Linux/UnrealEngine/Airsim/Airsim08192022/AirSim/Unreal/Environments/Blocks
+# /Blocks.uproject -game
+# cd /home/travis/Mount/Storage/SharedHDD/Linux/UnrealEngine/Airsim/Airsim08192022/AirSim/Unreal/Environments/Blocks
+# make BlocksEditor
+
+~/Mount/Storage/SharedHDD/Linux/UnrealEngine/Unreal427/UnrealEngine/Engine/Binaries/ThirdParty/Mono/Linux/bin/mono ~/Mount/Storage/SharedHDD/Linux/UnrealEngine/Unreal427/UnrealEngine/Engine/Binaries/DotNET/UnrealBuildTool.exe Development Linux -Project=/home/travis/Mount/Storage/SharedHDD/Linux/UnrealEngine/Airsim/Airsim08232022/AirSim/Unreal/Environments/Blocks/Blocks.uproject -TargetType=Editor -Progress
